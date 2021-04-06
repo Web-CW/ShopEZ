f51f5e83124fde3b15819b3a3f6ae3f50f9e969a
xaviernguyen07 <tnguyen436@my.bcit.ca>
2021-04-05 23:03:37 -0700

    finish category activity

---
+++ app/src/main/java/ca/bcit/shopez/CategoryActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.shopez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
