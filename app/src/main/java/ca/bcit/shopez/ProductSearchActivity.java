774f7cb8f3dea3654c8bffe790a507fdce68600a
weich <wei.charlie@hotmail.com>
2021-04-05 22:04:02 -0700

    testing product search page.

--- app/src/main/java/ca/bcit/shopez/ProductSearchActivity.java
+++ app/src/main/java/ca/bcit/shopez/ProductSearchActivity.java
@@ -2,15 +2,13 @@ package ca.bcit.shopez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ProductSearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_product_search);
     }
 }
--- app/src/main/java/ca/bcit/shopez/SignInActivity.java
+++ app/src/main/java/ca/bcit/shopez/SignInActivity.java
@@ -2,7 +2,9 @@ package ca.bcit.shopez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void products_activity_on_click(View view) {
+        Intent intent = new Intent(this, ProductSearchActivity.class);
+        startActivity(intent);
     }
 }
