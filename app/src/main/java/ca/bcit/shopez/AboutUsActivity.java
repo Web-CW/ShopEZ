e9f9a749579ffd89b2dbd1b2f94d71217d7d7936
weich <wei.charlie@hotmail.com>
2021-04-10 02:36:17 -0700

    about us constructed

--- app/src/main/java/ca/bcit/shopez/AboutUsActivity.java
+++ app/src/main/java/ca/bcit/shopez/AboutUsActivity.java
@@ -1,14 +1,71 @@
 package ca.bcit.shopez;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class AboutUsActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AboutUsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        Toolbar toolbar = findViewById(R.id.about_us_toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_about_us_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent intent = null;
+
+        switch(id) {
+            case R.id.nav_homepage:
+                intent = new Intent(this, ProductSearchActivity.class);
+                break;
+            case R.id.nav_view_cart:
+                intent = new Intent(this, MyCartActivity.class);
+                break;
+            case R.id.nav_sign_in:
+                intent = new Intent(this, SignInActivity.class);
+                break;
+            case R.id.nav_sign_out:
+                FirebaseAuth.getInstance().signOut(); // logout
+                intent = new Intent(getApplicationContext(), SignInActivity.class);
+                break;
+            case R.id.nav_about:
+                intent = new Intent(this, AboutUsActivity.class);
+                break;
+        }
+
+        startActivity(intent);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
