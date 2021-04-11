acc5df8cf2839c7fc20135d94d119f96b091e48e
weich <wei.charlie@hotmail.com>
2021-04-10 21:34:17 -0700

    menu edited

---
+++ app/src/main/java/ca/bcit/shopez/FAQActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.shopez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FAQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_a_q);
+    }
+}
